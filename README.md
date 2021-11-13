577a88c1e8648895cbf9e3ee82c7b3c803a1c8fa
bvinicius <vinicius.bazanella1@gmail.com>
2021-11-13 02:21:45 -0300

    Inicia projeto vue

--- README.md
+++ README.md
@@ -1,24 +1,33 @@
-# midi
+# MIDI Playground
 
-## Project setup
+### A Vue.js app to explore MIDI devices usage on browser.
+
+## Vue Setup
+
+### Project setup
+
 ```
 npm install
 ```
 
-### Compiles and hot-reloads for development
+#### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
-### Compiles and minifies for production
+#### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
-### Lints and fixes files
+#### Lints and fixes files
+
 ```
 npm run lint
 ```
 
-### Customize configuration
+#### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <span>MIDI</span>
+  </div>
+</template>
+
+<script>
+import MIDIService from "../modules/MIDIService";
+
+export default {
+  name: "Home",
+  data: () => ({
+    midi: {},
+    midimap: {},
+  }),
+  mounted: function () {
+    this.subscribeToMIDI();
+  },
+  methods: {
+    subscribeToMIDI: function () {
+      MIDIService.midiMessageSubject.subscribe((message) =>
+        this.onMIDIMessage(message)
+      );
+    },
+
+    onMIDIMessage: function (message) {
+      console.log(message);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
